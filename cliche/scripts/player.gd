59c3ab7342bbf41f95626a6c58d3aea2a3f631c5
Pavocracy <pavocracy@pm.me>
2022-11-17 21:15:02 +1000

    Fix isometric movement

--- cliche/scripts/player.gd
+++ cliche/scripts/player.gd
@@ -3,9 +3,11 @@ extends KinematicBody2D
 var sprite: Sprite
 var movement: Vector2
 var speed: int
+var tileSize: Dictionary
 
 func cartesian_to_isometric(cartesian: Vector2):
-	return Vector2(cartesian.x - cartesian.y, (cartesian.x + cartesian.y) / 2)
+	tileSize = Dictionary({"width": 223.0, "height": 128.0})
+	return Vector2(cartesian.x - cartesian.y, (cartesian.x + cartesian.y) / (tileSize["width"] / tileSize["height"]))
 	
 func process_input(): 
 	sprite = $Sprite
@@ -14,18 +16,18 @@ func process_input():
 	if Input.is_action_pressed("up"):
 		movement.y -= 1
 		sprite.frame = 1
+	if Input.is_action_pressed("right"):
+		movement.x += 1
+		sprite.frame = 0
 	if Input.is_action_pressed("down"):
 		movement.y += 1
 		sprite.frame = 3
 	if Input.is_action_pressed("left"):
 		movement.x -= 1
 		sprite.frame = 2
-	if Input.is_action_pressed("right"):
-		movement.x += 1
-		sprite.frame = 0
 	movement = movement.normalized() * speed
 	movement = cartesian_to_isometric(movement)
-	var _move = move_and_slide(movement)
+	movement = move_and_slide(movement)
 
 func _physics_process(_delta):
 	process_input()
