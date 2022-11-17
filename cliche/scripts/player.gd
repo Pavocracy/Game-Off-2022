179b486abd5afbee8124f517c62814836ddadd6e
Pavocracy <pavocracy@pm.me>
2022-11-17 22:34:00 +1000

    Make pig jump

--- cliche/scripts/player.gd
+++ cliche/scripts/player.gd
@@ -1,33 +1,61 @@
 extends KinematicBody2D
 
 var sprite: Sprite
-var movement: Vector2
+var cartesian: Vector2
+var isometric: Vector2
+var _moved: Vector2
 var speed: int
+var jumping: bool
+var jump_frame: int
+var jump_direction: String
 var tileSize: Dictionary
 
-func cartesian_to_isometric(cartesian: Vector2):
-	tileSize = Dictionary({"width": 223.0, "height": 128.0})
-	return Vector2(cartesian.x - cartesian.y, (cartesian.x + cartesian.y) / (tileSize["width"] / tileSize["height"]))
-	
-func process_input(): 
+func _ready():
 	sprite = $Sprite
-	movement = Vector2()
 	speed = 200
+	jumping = false
+	jump_frame = 0
+	tileSize = Dictionary({"width": 223.0, "height": 128.0})
+
+func cartesian_to_isometric(cartesian: Vector2):
+	isometric = Vector2()
+	isometric.x = cartesian.x - cartesian.y
+	isometric.y = (cartesian.x + cartesian.y) / (tileSize["width"] / tileSize["height"])
+	return isometric
+
+func check_movement():
+	if jumping:
+		return
+	cartesian = Vector2()
 	if Input.is_action_pressed("up"):
-		movement.y -= 1
+		cartesian.y -= 1
 		sprite.frame = 1
 	if Input.is_action_pressed("right"):
-		movement.x += 1
+		cartesian.x += 1
 		sprite.frame = 0
 	if Input.is_action_pressed("down"):
-		movement.y += 1
+		cartesian.y += 1
 		sprite.frame = 3
 	if Input.is_action_pressed("left"):
-		movement.x -= 1
+		cartesian.x -= 1
 		sprite.frame = 2
-	movement = movement.normalized() * speed
-	movement = cartesian_to_isometric(movement)
-	movement = move_and_slide(movement)
+	cartesian = cartesian.normalized() * speed
+	isometric = cartesian_to_isometric(cartesian)
+	_moved = move_and_slide(isometric)
+
+func check_jump():
+	# This is pretty bad. Find a better way
+	if Input.is_action_pressed("jump") && jumping == false:
+		jumping = true
+		jump_frame += 1
+	if jumping == true:
+		if jump_frame > 10:
+			jumping = false
+			jump_frame = 0
+			return
+		sprite.offset.y -= jump_frame if jump_frame <= 5 else -(jump_frame - 5)
+		jump_frame += 1
 
 func _physics_process(_delta):
-	process_input()
+	check_movement()
+	check_jump()
