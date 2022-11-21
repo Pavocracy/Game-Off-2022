ac925e5b40ab360972ab328c6b988964a15c45af
Pavocracy <pavocracy@pm.me>
2022-11-21 22:38:53 +1000

    Animate with new pig texture

--- cliche/scripts/player.gd
+++ cliche/scripts/player.gd
@@ -1,19 +1,17 @@
 extends KinematicBody2D
 
+const SPEED: int = 200
+const GRAVITY: int = 20
 var sprite: Sprite
+var animation: AnimationTree
 var cartesian: Vector2
 var isometric: Vector2
 var _moved: Vector2
-var speed: int
-var jumping: bool
-var jump_frame: int
 var tileSize: Dictionary
 
 func _ready():
 	sprite = $Sprite
-	speed = 200
-	jumping = false
-	jump_frame = 0
+	animation = $AnimationTree
 	tileSize = Dictionary({"width": 223.0, "height": 128.0})
 
 func cartesian_to_isometric(cartesian: Vector2):
@@ -26,33 +24,29 @@ func check_movement():
 	cartesian = Vector2()
 	if Input.is_action_pressed("up"):
 		cartesian.y -= 1
-		sprite.frame = 3
 	if Input.is_action_pressed("right"):
 		cartesian.x += 1
-		sprite.frame = 2
 	if Input.is_action_pressed("down"):
 		cartesian.y += 1
-		sprite.frame = 6
 	if Input.is_action_pressed("left"):
 		cartesian.x -= 1
-		sprite.frame = 7
-	cartesian = cartesian.normalized() * speed
+	cartesian = cartesian.normalized()
 	isometric = cartesian_to_isometric(cartesian)
-	_moved = move_and_slide(isometric)
+	if isometric:
+		animation.get("parameters/playback").travel("Walking")
+		animation.set("parameters/Idle/blend_position", isometric)
+		animation.set("parameters/Walking/blend_position", isometric)
+		_moved = move_and_slide(isometric * SPEED)
+	else:
+		animation.get("parameters/playback").travel("Idle")
 
 func check_jump():
-	# This is pretty bad. Find a better way
-	if Input.is_action_pressed("jump") && jumping == false:
-		jumping = true
-		jump_frame += 1
-	if jumping == true:
-		if jump_frame > 10:
-			jumping = false
-			jump_frame = 0
-			return
-		sprite.offset.y -= jump_frame if jump_frame <= 5 else -(jump_frame - 5)
-		jump_frame += 1
+	# TODO: Impliment jump using gravity
+	if Input.is_action_pressed("jump"):
+		pass
 
 func _physics_process(_delta):
 	check_movement()
+	
+func _input(_event):
 	check_jump()
